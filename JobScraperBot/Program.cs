c1ffc9f7f8f3cb9e6409d8f772d8a9ad3506b084
Юрій Козіцький <y.kozitski@gmail.com>
2024-12-25 20:08:21 +0200

    turned console app into library

--- JobScraperBot/Program.cs
+++ JobScraperBot/Program.cs
@@ -1,36 +1,36 @@
-﻿using JobScraperBot.Extensions;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using NLog;
+﻿//using JobScraperBot.Extensions;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using NLog;
 
-namespace JobScraperBot
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var logger = LogManager.GetCurrentClassLogger();
+//namespace JobScraperBot
+//{
+//    public class Program
+//    {
+//        public static async Task Main(string[] args)
+//        {
+//            var logger = LogManager.GetCurrentClassLogger();
 
-            try
-            {
-                var services = new ServiceCollection().AddBotServices();
+//            try
+//            {
+//                var services = new ServiceCollection().AddBotServices();
 
-                using var serviceProvider = services.BuildServiceProvider();
+//                using var serviceProvider = services.BuildServiceProvider();
 
-                var bot = new Bot(serviceProvider);
-                await bot.Run();
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                LogManager.Shutdown();
-            }
-        }
+//                var bot = new Bot(serviceProvider);
+//                await bot.Run();
+//            }
+//            catch (Exception ex)
+//            {
+//                logger.Error(ex, "Stopped program because of exception");
+//                throw;
+//            }
+//            finally
+//            {
+//                LogManager.Shutdown();
+//            }
+//        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args);
-    }
-}
+//        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args);
+//    }
+//}
