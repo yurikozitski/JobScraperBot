cfce2c41f7eac7819c2d0440a79a9a48a2c5ef01
Yuri Kozitski <y.kozitski@gmail.com>
2025-02-23 00:15:07 +0200

    Revert "comment change"
    This reverts commit dcb49e4b16b67122ac1726b6409e30c967b18a15.

--- JobScraperBot.DAL/Migrations/20241130185430_Initial.cs
+++ JobScraperBot.DAL/Migrations/20241130185430_Initial.cs
@@ -2,7 +2,7 @@
 
 #nullable disable
 
-#pragma warning disable CA1814 //Prefer jagged arrays over multidimensional
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
 namespace JobScraperBot.DAL.Migrations
 {
